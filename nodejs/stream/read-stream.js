d53965047dafd82982dd5798c04f40ffe92c78ab
Tuan Nguyen Minh <tuana9a@gmail.com>
2022-03-06 15:21:50 +0700

    npm audit fix

--- nodejs/stream/read-stream.js
+++ nodejs/stream/read-stream.js
@@ -1,18 +1,39 @@
-var fs = require("fs");  
-var data = '';  
-// Create a readable stream  
-var readerStream = fs.createReadStream('input.txt');  
-// Set the encoding to be utf8.   
-readerStream.setEncoding('UTF8');  
-// Handle stream events --> data, end, and error  
-readerStream.on('data', function(chunk) {  
-   data += chunk;  
-   console.log(chunk);
-});  
-readerStream.on('end',function(){  
-   console.log(data);  
-});  
-readerStream.on('error', function(err){  
-   console.log(err.stack);  
-});  
-console.log("execution end");
+const fs = require("fs");
+const inputPath = "resource/input.txt";
+
+function test1() {
+    var data = "";
+    // Create a readable stream
+    const readStream = fs.createReadStream(inputPath); // read full data
+    // Set the encoding to be utf8.
+    readStream.setEncoding("UTF8");
+    // Handle stream events --> data, end, and error
+    readStream.on("data", function (chunk) {
+        data += chunk;
+        console.log(`chunk: "${chunk}"`);
+    });
+    readStream.on("end", function () {
+        console.log("=================RESULT================");
+        console.log(data);
+    });
+    readStream.on("error", function (err) {
+        console.log(err.stack);
+    });
+    console.log("execution end");
+}
+test1();
+
+function test2() {
+    var data = "";
+    const readStream = fs.createReadStream(inputPath, { start: 10 }); // skip first 10 byte
+    // this start option make random access file with specific range
+    readStream.on("data", function (chunk) {
+        data += chunk;
+    });
+    readStream.on("end", function () {
+        console.log("=================RESULT================");
+        console.log(data);
+    });
+}
+
+test2();
