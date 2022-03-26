7eba705ce0118953d92613831737b34ea01d12e7
Tuan Nguyen Minh <tuana9a@gmail.com>
2022-03-26 21:58:23 +0700

    update: refactor code register-preview

--- python/register-preview.py
+++ python/register-preview.py
@@ -2,11 +2,11 @@ import os
 import time
 import json
 import pymongo
+import dotenv
+import openpyxl
 
-from openpyxl import load_workbook
-from dotenv import load_dotenv, find_dotenv
+dotenv.load_dotenv(dotenv.find_dotenv())
 
-load_dotenv(find_dotenv())
 adapter = {
     "Mã_lớp": "MaLop",
     "Mã_lớp_kèm": "MaLopKem",
@@ -35,25 +35,26 @@ prop_table = {
     "GhiChu": 0,
 }
 
-semester = os.getenv("SEMESTER")
-db_name = os.getenv("DATABASE_NAME")
-xlsx_path = os.getenv("XLSX_PATH")
-conn_str = os.getenv("CONNECTION_STRING")
-head_row = int(os.getenv("HEAD_ROW"))
-start_data_row = int(os.getenv("START_DATA_ROW"))
+SEMESTER = os.getenv("SEMESTER")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+XLSX_FILEPATH = os.getenv("XLSX_PATH")
+CONNECTION_STRING = os.getenv("CONNECTION_STRING")
+HEAD_ROW = int(os.getenv("HEAD_ROW"))
+START_DATA_ROW = int(os.getenv("START_DATA_ROW"))
 
-client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+client = pymongo.MongoClient(CONNECTION_STRING, serverSelectionTimeoutMS=5000)
 
-wb = load_workbook(xlsx_path)
+wb = openpyxl.load_workbook(XLSX_FILEPATH)
 ws = wb.active
 max_column = ws.max_column
 max_row = ws.max_row
 
+print("semester = ", SEMESTER)
 print("max_column =", max_column)
 print("max_row =", max_row)
 
 # determine column name then map it to prop table
-for row in ws.iter_rows(min_row=head_row, max_row=head_row, values_only=True):
+for row in ws.iter_rows(min_row=HEAD_ROW, max_row=HEAD_ROW, values_only=True):
     i = 0
     for cell in row:
         prop_name = adapter.get(cell)
@@ -61,7 +62,10 @@ for row in ws.iter_rows(min_row=head_row, max_row=head_row, values_only=True):
             prop_table[prop_name] = i
         i = i + 1
 
-print(prop_table)
+print("=== prop_table ===")
+
+for key in prop_table:
+    print(key, "=", prop_table[key])
 
 
 class SchoolClass:
@@ -69,6 +73,7 @@ class SchoolClass:
         for key in prop_table:
             self.__setattr__(key, row[prop_table[key]])
         self.created = int(time.time() * 1000)
+        self.semester = SEMESTER
         pass
 
     def __str__(self) -> str:
@@ -76,7 +81,8 @@ class SchoolClass:
         for key in prop_table:
             result = result + key + ": " + \
                 str(self.__getattribute__(key)) + ", "
-        result += "create: " + str(self.created)
+        result += "semester: " + str(self.semester)
+        result += "created: " + str(self.created)
         return result
 
     def to_dict(self):
@@ -84,13 +90,14 @@ class SchoolClass:
         for key in prop_table:
             result[key] = self.__getattribute__(key)
         result["created"] = self.created
+        result["semester"] = self.semester
         return result
 
 
 count = 0
 stop = True
 
-for row in ws.iter_rows(min_row=start_data_row, max_row=start_data_row, values_only=True):
+for row in ws.iter_rows(min_row=START_DATA_ROW, max_row=START_DATA_ROW, values_only=True):
     # school_class = create_school_class(row)
     school_class = SchoolClass(row)
     print(school_class)
@@ -101,10 +108,10 @@ for row in ws.iter_rows(min_row=start_data_row, max_row=start_data_row, values_o
 if (not stop):
     count = 0
     classes = []
-    for row in ws.iter_rows(min_row=start_data_row, values_only=True):
+    for row in ws.iter_rows(min_row=START_DATA_ROW, values_only=True):
         count = count + 1
         school_class = SchoolClass(row)
         classes.append(school_class.to_dict())
-    result = client[db_name]["classes." + semester].insert_many(classes)
+    result = client[DATABASE_NAME]["school.classes"].insert_many(classes)
     print('count =', count)
     print('inserted = ', len(result.inserted_ids))
