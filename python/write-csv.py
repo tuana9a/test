b6d600bb6b1ecd7f1a05e1bc8f3a0b4c6c64113c
Tuan Nguyen Minh <tuana9a@gmail.com>
2022-03-29 15:32:21 +0700

    add filepath variable

--- python/write-csv.py
+++ python/write-csv.py
@@ -1,21 +1,24 @@
 import csv
 import random
 
-first_names = ('John', 'Andy', 'Joe', 'Locus', 'Nice', 'Hello', 'Me',
-               'Ha', 'World', 'Morona', 'Miruke', 'Ymaki', 'Mel', 'Kina', 'Repu')
-last_names = ('Johnson', 'Smith', 'Williams', 'Ali', 'Beatriz',
-              'Charles', 'Diya', 'Eric', 'Fatima', 'Gabriel', 'Hanna')
-role_names = ('Admin', 'User', 'Guest')
+fnames = ('John', 'Andy', 'Joe', 'Locus', 'Nice', 'Hello', 'Me',
+          'Ha', 'World', 'Morona', 'Miruke', 'Ymaki', 'Mel', 'Kina', 'Repu')
+
+lnames = ('Johnson', 'Smith', 'Williams', 'Ali', 'Beatriz',
+          'Charles', 'Diya', 'Eric', 'Fatima', 'Gabriel', 'Hanna')
+
+roles = ('Admin', 'User', 'Guest')
 
 
 header = ['First Name', 'Last Name', 'Email', 'Role']
+filepath = 'countries.local.csv'
 
-with open('countries.local.csv', 'w', encoding='UTF8') as f:
+with open(filepath, 'w', encoding='UTF8') as f:
     writer = csv.writer(f)
     # write the header
     writer.writerow(header)
     for i in range(10_000):
-        data = [random.choice(first_names) + " " + random.choice(last_names), random.choice(
-            first_names) + " " + random.choice(last_names), 'example@example.com', random.choice(role_names)]
+        data = [random.choice(fnames) + " " + random.choice(lnames), random.choice(
+            fnames) + " " + random.choice(lnames), 'example@example.com', random.choice(roles)]
         # write the data
         writer.writerow(data)
